4e34aae9a2fd8d724333955eedcc70518640edda
João <jocolognesi@gmail.com>
2025-04-17 17:49:57 -0300

    get_cmd_path refactor

--- src/get_cmd_path.c
+++ src/get_cmd_path.c
@@ -6,13 +6,13 @@
 /*   By: jcologne <jcologne@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:53:18 by jcologne          #+#    #+#             */
-/*   Updated: 2025/04/17 17:03:52 by jcologne         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:47:32 by jcologne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*check_path_for_cmd(const char *dir_path, const char *cmd)
+static char	*check_path_for_cmd(const char *dir_path, const char *cmd)
 {
 	char	*dir_slash;
 	char	*full_path;
@@ -30,7 +30,7 @@ char	*check_path_for_cmd(const char *dir_path, const char *cmd)
 	return (NULL);
 }
 
-char	**get_path_directories(void)
+static char	**get_path_directories(void)
 {
 	char	*path_env;
 	char	**paths;
@@ -42,6 +42,22 @@ char	**get_path_directories(void)
 	if (!paths)
 		return (NULL);
 	return (paths);
+}
+
+static char	*check_direct_path(char *cmd)
+{
+	if (ft_strchr(cmd, '/') != NULL)
+	{
+		if (access(cmd, F_OK) == -1)
+			return (NULL);
+		if (access(cmd, X_OK) == -1)
+		{
+			g_exit_status = PERM_DENIED;
+			return (NULL);
+		}
+		return (ft_strdup(cmd));
+	}
+	return (NULL);
 }
 
 /**
@@ -56,17 +72,9 @@ char	*get_cmd_path(char *cmd)
 	char	*full_path;
 	int		i;
 
-	if (ft_strchr(cmd, '/') != NULL)
-	{
-		if (access(cmd, F_OK) == -1)
-			return (NULL);
-		if (access(cmd, X_OK) == -1)
-		{
-			g_exit_status = PERM_DENIED;
-			return (NULL);
-		}
-		return (ft_strdup(cmd));
-	}
+	full_path = check_direct_path(cmd);
+	if (full_path)
+		return (full_path);
 	paths = get_path_directories();
 	if (!paths)
 		return (NULL);
