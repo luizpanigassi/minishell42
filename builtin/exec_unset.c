075377b32bdb8f8776c4712af6c7876b5413b321
Luiz Fernando Nascimento Panigassi <luinasci@student.42sp.org.br>
2025-04-21 16:01:08 -0300

    exec_unset refactored

--- builtin/exec_unset.c
+++ builtin/exec_unset.c
@@ -6,59 +6,81 @@
 /*   By: luinasci <luinasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:51:59 by luinasci          #+#    #+#             */
-/*   Updated: 2025/04/21 14:52:14 by luinasci         ###   ########.fr       */
+/*   Updated: 2025/04/21 15:48:05 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	handle_unset_error(char *arg)
+{
+	if (is_valid_var_name(arg))
+		return (0);
+	ft_putstr_fd("minishell: unset: '", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+	return (1);
+}
+
+static void	shift_environment(char **env_ptr)
+{
+	char	**ptr;
+
+	ptr = env_ptr;
+	while (*ptr)
+	{
+		*ptr = *(ptr + 1);
+		ptr++;
+	}
+}
+
+static void	remove_env_var(const char *var_name)
+{
+	extern char	**environ;
+	char		**env_ptr;
+	char		*eq;
+	size_t		var_len;
+	size_t		name_len;
+
+	env_ptr = environ;
+	name_len = ft_strlen(var_name);
+	while (*env_ptr)
+	{
+		eq = ft_strchr(*env_ptr, '=');
+		if (eq)
+			var_len = eq - *env_ptr;
+		else
+			var_len = ft_strlen(*env_ptr);
+		if (var_len == name_len && ft_strncmp(*env_ptr, var_name, var_len) == 0)
+		{
+			free(*env_ptr);
+			shift_environment(env_ptr);
+			break ;
+		}
+		env_ptr++;
+	}
+}
 
 /**
  * @brief Executes the 'unset' builtin to remove environment variables.
  * @param args Array of arguments (e.g., ["unset", "VAR"]).
  * @return 0 on success, 1 if any invalid variable name is encountered.
  */
-int exec_unset(char **args)
+int	exec_unset(char **args)
 {
-	extern char **environ;
-	int ret;
-	int i;
+	int	ret;
+	int	i;
 
 	if (!args[1])
 		return (0);
-
 	ret = 0;
 	i = 1;
 	while (args[i])
 	{
-		if (!is_valid_var_name(args[i]))
-		{
-			ft_putstr_fd("minishell: unset: '", STDERR_FILENO);
-			ft_putstr_fd(args[i], STDERR_FILENO);
-			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+		if (handle_unset_error(args[i]))
 			ret = 1;
-			i++;
-			continue;
-		}
-
-		char **env_ptr = environ;
-		while (*env_ptr)
-		{
-			char *eq = ft_strchr(*env_ptr, '=');
-			size_t var_len = eq ? eq - *env_ptr : ft_strlen(*env_ptr);
-
-			if (ft_strncmp(*env_ptr, args[i], var_len) == 0 && var_len == ft_strlen_size(args[i]))
-			{
-				free(*env_ptr);
-				char **ptr = env_ptr;
-				while (*ptr)
-				{
-					*ptr = *(ptr + 1);
-					ptr++;
-				}
-				break;
-			}
-			env_ptr++;
-		}
+		else
+			remove_env_var(args[i]);
 		i++;
 	}
 	return (ret);
